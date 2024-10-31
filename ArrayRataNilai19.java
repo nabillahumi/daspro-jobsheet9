81e7c02f2a7d43b7f0a88febf71146f600f9ab0a
nabillahumi <nabillahumi003@gmail.com>
2024-10-31 13:48:35 +0700

    Array 1 Jobsheet 9

--- ArrayRataNilai19.java
+++ ArrayRataNilai19.java
@@ -8,6 +8,7 @@ public class ArrayRataNilai19 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke- " +(i+1)+ " : ");
@@ -15,9 +16,13 @@ public class ArrayRataNilai19 {
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] >70) {
+                jumlahLulus++;
+            }
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-ratanilai = " +rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " +jumlahLulus);
 
     }
 }
