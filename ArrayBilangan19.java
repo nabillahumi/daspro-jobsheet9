84e15366f0a85d472309ee7ecda3db6ee67f076d
nabillahumi <nabillahumi003@gmail.com>
2024-10-31 09:06:04 +0700

    Array 1 Jobsheet 9

--- ArrayBilangan19.java
+++ ArrayBilangan19.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class ArrayBilangan19 {
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         int [] bil = new int [4];
         bil [0] = 5;
